9db81f22a1bed29a144d7d1bc84fa031b9d4a78c
Paige Saint Germain <paigejerin@gmail.com>
2025-02-10 17:39:15 -0500

    completed assignment 08.

--- js/assignment.js
+++ js/assignment.js
@@ -1,8 +1,54 @@
-"use strict";
+'use strict';
 
-const submissionBtn = document.getElementById("submission-btn");
-const smallestNumberElement = document.getElementById("smallest-number");
-const largestNumberElement = document.getElementById("largest-number");
-const averageNumberElement = document.getElementById("average-number");
+const submissionBtn = document.getElementById('submission-btn');
+const smallestNumberElement = document.getElementById('smallest-number');
+const largestNumberElement = document.getElementById('largest-number');
+const averageNumberElement = document.getElementById('average-number');
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+const findSmallestNumber = (myNumbers) => {
+    let smallest = undefined;
+
+    myNumbers.forEach((element) => {
+        if (smallest == undefined || smallest > element) {
+            smallest = element;
+        }
+    });
+
+    return smallest;
+};
+
+const findLargestNumber = (myNumbers) => {
+    let largest = 0;
+
+    myNumbers.forEach((element) => {
+        if (largest < element) {
+            largest = element;
+        }
+    });
+
+    return largest;
+};
+
+const findAverage = (myNumbers) => {
+    let sum = 0;
+
+    myNumbers.forEach((element) => {
+        sum = sum + element;
+    });
+
+    let average = sum / myNumbers.length;
+
+    return average;
+};
+
+const render = () => {
+    smallestNumberElement.innerHTML = findSmallestNumber(myNumbers);
+    largestNumberElement.innerHTML = findLargestNumber(myNumbers);
+    averageNumberElement.innerHTML = findAverage(myNumbers);
+};
+
+submissionBtn.addEventListener('click', function () {
+    render();
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
-// Prevent us from attempting to use variables
-// that are not declared
-"use strict"
+// // Prevent us from attempting to use variables
+// // that are not declared
+// 'use strict';
+
+// let starters = ['Bulbasaur', 'Squirtle', 'Charmander'];
+
+// // Create an array and loop through it, logging out each value using the basic loop syntax.
+// for (let index = 0; index < starters.length; index++) {
+//     console.log(starters[index]);
+// }
+
+// // Create an array and loop through it, logging out each value using the "for...of" syntax.
+// for (const starter of starters) {
+//     console.log(starter);
+// }
+
+// // Create an array and loop through it, logging out each value using the Array.forEach() method.
+// starters.forEach(starter => {
+//     console.log(starter);
+// });
+
+// // Loop through an array using "for...of" and try assigning all the elements a new value. What happens?
+// for (let starter of starters) {
+//     starter = 'pikachu';
+//     console.log(starter);
+// }
+
+// console.log(starters); // Nothing seems to happen. Even with the array and loop using LET.
+
+// // Loop through an array using the Array.forEach() method and try assigning all the elements a new value. What happens?
+// starters.forEach(starter => {
+//     starter = 'pikachu';
+//     console.log(starter);
+// });
+
+// console.log(starters); // Nothing seems to happen. I googled it and I think I would need to use map()
+
